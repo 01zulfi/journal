0956aeecf7820222fe31f666161fa07abaa97986
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-05-21 20:28:35 +0500

    Update App component * Bring together all of the components * Set up routes with react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React, { FC } from 'react';
+import Header from './components/Header';
+import JournalsList from './components/JournalsList';
+import Journal from './components/Journal';
+import { Routes, Route } from 'react-router-dom';
 
 const App: FC = function App() {
-  return <h2>Hello World</h2>;
+  return (
+    <section>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<JournalsList />}></Route>
+        <Route path="/:urlName" element={<Journal />}></Route>
+      </Routes>
+    </section>
+  );
 };
 
 export default App;
