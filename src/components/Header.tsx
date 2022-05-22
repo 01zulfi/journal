6ca91ad097ec07793d35d1bad93477c1770ba9ee
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-05-22 21:09:42 +0500

    Style heading

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,14 @@
 import React, { FC } from 'react';
 
 const Header: FC = function Header() {
-  return <h1 className="underline font-bold m-4">zulfi's journal</h1>;
+  return (
+    <div className="flex py-4 gap-1">
+      <h1 className="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-l from-fg-green to-fg-cyan tracking-wide">
+        zulfi's journal
+      </h1>
+      <span className="text-2xl font-extrabold blinking-cursor w-[10px]"></span>
+    </div>
+  );
 };
 
 export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bg-primary': '#08070b',
+        'fg-primary': '#8f9ba8',
+        'fg-secondary': '#f2f2f2',
+        'fg-cyan': '#80ffea',
+        'fg-green': '#50fa7b',
+      },
+    },
   },
   plugins: [],
 };
