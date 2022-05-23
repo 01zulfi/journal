c0e182f32bae0bca0c5b798191d1fa4bb16242ea
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-05-23 19:53:54 +0500

    Convert homepage title into a link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
 import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 const Header: FC = function Header() {
   return (
     <div className="flex py-4 gap-1">
       <h1 className="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-l from-fg-green to-fg-cyan tracking-wide">
-        zulfi's journal
+        <Link to="/">zulfi's journal</Link>
       </h1>
       <span className="text-2xl font-extrabold blinking-cursor w-[10px]"></span>
     </div>
