17d87308235301cbc060e4fa2ff613647cee4154
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-05-25 17:06:11 +0500

    Install react-syntax-highlighter; start styling journal content

--- src/components/JournalContent.tsx
+++ src/components/JournalContent.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from 'react';
 import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import nord from 'react-syntax-highlighter/dist/esm/styles/prism/nord';
 
 interface JournalContentProps {
   content: string;
@@ -10,7 +12,29 @@ const JournalContent: FC<JournalContentProps> = function JournalContent({
 }) {
   return (
     <div className="journal-content">
-      <ReactMarkdown>{content}</ReactMarkdown>
+      <ReactMarkdown
+        children={content}
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || '');
+            return !inline && match ? (
+              <SyntaxHighlighter
+                children={String(children).replace(/\n$/, '')}
+                language={match[1]}
+                showLineNumbers
+                style={nord}
+                customStyle={{ margin: '2rem 0' }}
+                PreTag="div"
+                {...props}
+              />
+            ) : (
+              <code className={`className inline-code`} {...props}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      ></ReactMarkdown>
     </div>
   );
 };
